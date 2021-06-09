95127524fe9a977aa3712c829762c5f26a648b7d
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2021-06-09 21:32:33 +1000

    add markdown

--- init.el
+++ init.el
@@ -5,3 +5,13 @@
 
 (require 'misc-config)
 ;; (use-package proxy-config)
+
+;; Markdown
+(use-package
+  markdown-mode
+  :ensure t
+  :straight t
+  :config
+  :mode "\\.md$"
+  :interpreter "markdown-mode"
+  )
