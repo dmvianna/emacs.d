8087e660403945ea32c30ef9e45302f54de5996f
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2021-06-17 22:00:54 +1000

    tidy custom-set-variables; rm broken haskell-config.el

--- init.el
+++ init.el
@@ -46,27 +46,25 @@
   :commands lsp-ui-mode
   :hook (haskell-lsp-ui
          python-lsp-ui)
-  :config
-  (custom-set-variables
-   (lsp-ui-sideline-show-diagnostics t)
-   (lsp-ui-sideline-show-hover t)
-   (lsp-ui-sideline-show-code-actions t)
-   (lsp-ui-sideline-update-mode t)
-   (lsp-ui-peek-enable t)
-   (lsp-ui-peek-show-directory t)
-   (lsp-ui-doc-enable t)
-   (lsp-ui-doc-position 'bottom)
-   (lsp-ui-doc-show-with-cursor t)
-   (lsp-ui-doc-show-with-mouse t)
-   (lsp-ui-imenu-auto-refresh t)
-   (lsp-ui-imenu-window-width 40)
-   ))
+  :custom
+  (lsp-ui-sideline-show-diagnostics t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-sideline-show-code-actions t)
+  (lsp-ui-sideline-update-mode t)
+  (lsp-ui-peek-enable t)
+  (lsp-ui-peek-show-directory t)
+  (lsp-ui-doc-enable t)
+  (lsp-ui-doc-position 'bottom)
+  (lsp-ui-doc-show-with-cursor t)
+  (lsp-ui-doc-show-with-mouse t)
+  (lsp-ui-imenu-auto-refresh t)
+  (lsp-ui-imenu-window-width 40)
+  )
 
 (use-package lsp-haskell
   :ensure t
   :straight t
-  :hook (haskell-mode haskell-config)
-  )
+  :hook haskell-mode)
 
 (use-package lsp-python-ms
   :ensure t
