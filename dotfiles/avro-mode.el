3f27b6cddfd9a15d3be7b5f9b6f78a80693fab73
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2021-08-20 10:46:38 +1000

    rm all hardcoded types

--- dotfiles/avro-mode.el
+++ dotfiles/avro-mode.el
@@ -20,42 +20,15 @@
 
 (require 'generic-x)
 
-(defun make-avro-keywords (types)
-  (concat "\\<" (regexp-opt types) "\\>"))
-
-(setq avro-primitive-types
-      (make-avro-keywords
-       '("null" "boolean" "int" "long" "float" "double" "bytes" "string")))
-
-(setq avro-complex-types
-      (make-avro-keywords
-       '("record" "enum" "array" "map" "union" "alias")))
-
-(setq avro-reserved-words
-      '("protocol" "namespace"))
-
-(setq avro-delimiters
-  '("<" ">" ";" "{" "}"))
-
-(setq avro-comment
-  '("//"))
-
 (define-generic-mode
     'avro-mode
-  avro-comment
-  avro-reserved-words
-  ;; '(((make-avro-keywords avro-primitive-types) . font-lock-builtin-face)
-  ;;   ((make-avro-keywords avro-complex-types) . font-lock-type-face)
-  ;;   ((make-avro-keywords avro-reserved-words) . font-lock-keyword-face)
-  ;;   ((regexp-opt 'avro-delimiters) . font-lock-operator)
-  ;;   (("array") . font-lock-keyword-face)
-  ;;   )
- '(("\\<\\(?:b\\(?:oolean\\|ytes\\)\\|double\\|float\\|int\\|long\\|null\\|string\\)\\>"
-     . font-lock-builtin-face)
-    ("\\<\\(?:a\\(?:lias\\|rray\\)\\|enum\\|map\\|record\\|union\\)\\>" . font-lock-type-face)
+  '("//") ;; comments
+  '("protocol" "namespace") ;; reserved words
+  '(("\\<[[:alnum:]]+\\>" (0 font-lock-builtin-face) ("\\<[[:alnum:]]+\\>;?\\( {\\)?" nil nil (0 nil))) ;; type
     ("\\<\\(?:namespace\\|protocol\\)\\>" . font-lock-keyword-face)
-    ("{\\|}\\|;\\|<\\|>" . font-lock-constant-face))
-  '("\\.avdl$")
+    
+    ("{\\|}\\|;\\|<\\|>" . font-lock-constant-face)) ;; separators
+  '("\\.avdl$") ;; file extension
   nil
   "Major mode for editing Avro IDL files."
   )
