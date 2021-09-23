b102ecdec60ccbb4ce81df88b7845733c70f5b83
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2021-09-23 15:07:53 +1000

    add a whole bunch of stuff to improve JSX/TSX

--- init.el
+++ init.el
@@ -235,6 +235,27 @@
                 (require 'tide)
                 (tide-setup)))))
 
+(use-package web-mode
+  :straight t
+  :mode (("\\.html?\\'" . web-mode)
+         ;; ("\\.tsx\\'" . web-mode)
+         ("\\.jsx\\'" . web-mode))
+  :custom
+  (web-mode-markup-indent-offset 2)
+  (web-mode-css-indent-offset 2)
+  (web-mode-code-indent-offset 2)
+  (web-mode-block-padding 2)
+  (web-mode-comment-style 2)
+  (web-mode-enable-css-colorization t)
+  (web-mode-enable-auto-pairing t)
+  (web-mode-enable-comment-keywords t)
+  (web-mode-enable-current-element-highlight t)
+  :hook
+  ((web-mode . (lambda ()
+                  (require 'tide)
+                  (tide-setup)))
+   (web-mode . lsp)))
+
 (use-package prettier-js
   :straight t
   :commands (prettier-js-mode prettier)
@@ -243,16 +264,33 @@
   (prettier-js-args
    '("--trailing-comma" "all" "--single-quote" "--semi" "--arrow-parens" "always"))
   :hook ((js-mode . prettier-js-mode)
-         (typescript-mode . prettier-js-mode)))
+         (typescript-mode . prettier-js-mode)
+         (web-mode . prettier-js-mode)))
 
 (use-package typescript-mode
   :straight t
-  :mode "\\.tsx?\\'"
+  :init
+  (define-derived-mode typescript-tsx-mode typescript-mode "tsx")
+  :custom
+  (typescript-indent-level 2)
   :hook
-  ((typescript-mode . lsp)
-   (typescript-mode . (lambda ()
-                        (require 'tide)
-                        (tide-setup)))))
+  ((typescript-mode . subword-mode)
+   (typescript-mode . lsp))
+  :mode
+  ("\\.tsx?\\'" . typescript-tsx-mode))
+
+(use-package tree-sitter
+  :straight t
+  :hook
+  ((typescript-mode . tree-sitter-hl-mode)
+	 (typescript-tsx-mode . tree-sitter-hl-mode)))
+
+(use-package tree-sitter-langs
+  :straight t
+  :after tree-sitter
+  :config
+  (tree-sitter-require 'tsx)
+  (add-to-list 'tree-sitter-major-mode-language-alist '(typescript-tsx-mode . tsx)))
 
 (use-package tide
   :straight t
