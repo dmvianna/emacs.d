ac1d12abaa3386eb4088192be457aad0b2000468
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2021-10-07 16:32:08 +1100

    fix C-. in lsp-haskell

--- init.el
+++ init.el
@@ -82,7 +82,7 @@
 ;; LSP
 (use-package lsp-mode
   :straight t
-  :init (setq lsp-keymap-prefix "C-c l")
+  :bind ("C-c l" . lsp-keymap-prefix)
   :commands (lsp lsp-deferred))
 
 (use-package lsp-ui
@@ -129,12 +129,12 @@
   :config
   (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
   (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
-  (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
-  (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+  ;; (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+  ;; (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
   (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
   (define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
   (define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
-  (define-key haskell-mode-map (kbd "M-.") 'haskell-mode-jump-to-def)
+  ;; (define-key haskell-mode-map (kbd "M-.") 'haskell-mode-jump-to-def)
 )
 
 (use-package haskell-interactive-mode
