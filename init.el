2d6e770004bbb97f07c8ad6cf29575078c06afc7
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2021-10-27 12:56:57 +1100

    add nix support

--- init.el
+++ init.el
@@ -213,6 +213,18 @@
                           'python-pyright
                           'python-pylint))))
 
+;; nix
+(use-package nix-mode
+  :straight t
+  :init
+  (add-to-list 'lsp-language-id-configuration '(nix-mode . "nix"))
+  (lsp-register-client
+   (make-lsp-client :new-connection (lsp-stdio-connection '("rnix-lsp"))
+                    :major-modes '(nix-mode)
+                    :server-id 'nix))
+  :mode "\\.nix\\'")
+
+
 ;; Racket
 (use-package racket-mode
   :straight t
