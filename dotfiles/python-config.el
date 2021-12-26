780d120a9439124877f697fbf92d673b9d235c85
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2021-12-26 18:28:11 +1100

    fix flycheck in jedi-lsp

--- dotfiles/python-config.el
+++ dotfiles/python-config.el
@@ -17,8 +17,9 @@
   :config
   (with-eval-after-load "lsp-mode"
     (add-to-list 'lsp-disabled-clients 'pyls))
-    ;; (add-to-list 'lsp-enabled-clients 'jedi)
-    
+  ;; (add-to-list 'lsp-enabled-clients 'jedi)
+  (require 'lsp-diagnostics)
+  (lsp-diagnostics-flycheck-enable)
   :hook (python-mode . (lambda ()
                          (require 'lsp-jedi)
                          (lsp)
