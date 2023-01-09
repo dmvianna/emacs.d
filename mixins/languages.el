6b73dcb56326110b35d07528c1c4e5155a0f58c8
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2023-01-09 11:17:20 +1100

    rm dap-mode, no idea how to use it

--- mixins/languages.el
+++ mixins/languages.el
@@ -34,12 +34,6 @@
 ;; Posframe is a pop-up tool that must be manually installed for dap-mode
 (use-package posframe)
 
-;; Use the Debug Adapter Protocol for running tests and debugging
-(use-package dap-mode
-  :hook
-  (lsp-mode . dap-mode)
-  (lsp-mode . dap-ui-mode))
-
 ;;; languages
 
 (use-package avro-mode
