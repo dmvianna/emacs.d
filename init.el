18c91379b79f4f9417278f3a30662400af8f3e72
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2023-01-11 13:33:32 +1100

    set linum-mode globally until I figure out how to do it in prog-mode

--- init.el
+++ init.el
@@ -198,13 +198,14 @@
 (global-set-key (kbd "<home>") 'beginning-of-buffer)
 (global-set-key (kbd "<end>") 'end-of-buffer)
 
-;; Display line numbers in programming mode
+;; Display line numbers
+(global-linum-mode t)
+
 (use-package prog-mode
   :straight nil
   :config
   ;; open pairs with extra newline in between, and autoindent
   (electric-pair-local-mode t)
-  (display-line-numbers-mode t)
   :init
   (add-to-list 'auto-mode-alist '("\\.list$" . prog-mode)))
 
