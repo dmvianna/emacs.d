dcb1d67f069827e9b90ceef60306b723d01f66d3
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2023-03-29 10:47:45 +1100

    use dired for the time being, it works with lsp

--- mixins/early-modes.el
+++ mixins/early-modes.el
@@ -4,7 +4,30 @@
 ;;; to work properly.
 ;;; Code:
 
+;; direnv
+(use-package direnv
+    :config
+    ;; enable globally
+    (direnvmode)
+    ;; exceptions
+    ;; (add-to-list 'direnv-non-file-modes 'foobar-mode)
+
+    ;; nix-shells make too much spam -- hide
+    (setq direnv-always-show-summary nil)
+
+    :hook
+    ;; ensure direnv updates before flycheck and lsp
+    ;; https://github.com/wbolster/emacs-direnv/issues/17
+    (flycheck-before-syntax-check . direnv-update-environment)
+    (lsp-before-open-hook . direnv-update-environment)
+
+    :custom
+    ;; quieten logging
+    (warning-suppress-types '((direnv))))
+
+
 (use-package envrc
+ :disabled t
  :config (envrc-global-mode))
 
 (provide 'early-modes)
