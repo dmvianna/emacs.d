6bcb8e17f3d094e4884765371a1460950df99b8e
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2023-03-30 16:58:40 +1100

    bind comint-*-input in sbt-mode

--- mixins/languages/scala-config.el
+++ mixins/languages/scala-config.el
@@ -10,6 +10,8 @@
 ;; Enable sbt mode for executing sbt commands
 (use-package sbt-mode
   :commands sbt-start sbt-command
+  :bind (("C-p" . comint-previous-input)
+         ("C-n" . comint-next-input))
   :config
   ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
   ;; allows using SPACE when in the minibuffer
