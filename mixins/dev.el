40d6373668e1656f5f3ff700cf55b202ed8a4951
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2023-03-31 19:22:15 +1100

    clean up vundo config

--- mixins/dev.el
+++ mixins/dev.el
@@ -64,30 +64,11 @@
 
   :straight (vundo :type git :host github :repo "casouri/vundo")
 
-  :config
-  ;; Take less on-screen space.
-  (setq vundo-compact-display t)
-
   ;; Better contrasting highlight.
   (custom-set-faces
     '(vundo-node ((t (:foreground "#808080"))))
     '(vundo-stem ((t (:foreground "#808080"))))
-    '(vundo-highlight ((t (:foreground "#FFFF00")))))
-
-  ;; Use `HJKL` VIM-like motion, also Home/End to jump around.
-  (define-key vundo-mode-map (kbd "l") #'vundo-forward)
-  (define-key vundo-mode-map (kbd "<right>") #'vundo-forward)
-  (define-key vundo-mode-map (kbd "h") #'vundo-backward)
-  (define-key vundo-mode-map (kbd "<left>") #'vundo-backward)
-  (define-key vundo-mode-map (kbd "j") #'vundo-next)
-  (define-key vundo-mode-map (kbd "<down>") #'vundo-next)
-  (define-key vundo-mode-map (kbd "k") #'vundo-previous)
-  (define-key vundo-mode-map (kbd "<up>") #'vundo-previous)
-  (define-key vundo-mode-map (kbd "<home>") #'vundo-stem-root)
-  (define-key vundo-mode-map (kbd "<end>") #'vundo-stem-end)
-  (define-key vundo-mode-map (kbd "q") #'vundo-quit)
-  (define-key vundo-mode-map (kbd "C-g") #'vundo-quit)
-  (define-key vundo-mode-map (kbd "RET") #'vundo-confirm))
+    '(vundo-highlight ((t (:foreground "#FFFF00"))))))
 
 ;;; exclude the following directories from grep searches
 (eval-after-load 'grep
