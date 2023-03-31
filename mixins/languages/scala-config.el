8df432bee8e549a3f5fd1bd064d672ac7f9bb571
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2023-03-31 22:47:38 +1100

    finally fix sbt movement and environment.
    sbt will start at the directory closest to / that contains some
    of its droppings. So if I ever open sbt in $HOME, that's where
    the bastard will open its repl.

--- mixins/languages/scala-config.el
+++ mixins/languages/scala-config.el
@@ -11,19 +11,18 @@
 ;; Enable sbt mode for executing sbt commands
 (use-package sbt-mode
   :commands sbt-start sbt-command
-  :bind (:map sbt-mode-map
-         ("C-p" . comint-previous-input)
-         ("C-n" . comint-next-input))
-  :hook (sbt-mode . envrc-mode)
   :config
+  (defun fix-sbt-movement ()
+    (local-set-key (kbd "C-p") 'comint-previous-input)
+    (local-set-key (kbd "C-n") 'comint-next-input))
   ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
   ;; allows using SPACE when in the minibuffer
   (substitute-key-definition
    'minibuffer-complete-word
    'self-insert-command
-   minibuffer-local-completion-map
-   ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
-   (setq sbt:program-options '("-Dsbt.supershell=false"))))
+   minibuffer-local-completion-map)
+  (setq sbt:program-options '("-Dsbt.supershell=false"))
+  :hook (sbt-mode . fix-sbt-movement))
 
 (use-package lsp-metals
   :custom
