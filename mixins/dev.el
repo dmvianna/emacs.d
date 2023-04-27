56f31b25a591fdf6b382fc7412cba8ab76cb5e14
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2023-04-27 13:47:47 +1000

    disable company

--- mixins/dev.el
+++ mixins/dev.el
@@ -22,14 +22,14 @@
   :custom
   (flycheck-disabled-checkers '(haskell-stack-ghc haskell-ghc haskell-hlint)))
 
-;; Company -- text completion
-(use-package company
-  :delight
-  :config
-  (setq company-idle-delay 0.3
-        company-minimum-prefix-length 1
-        company-dabbrev-ignore-case nil)
-  (global-company-mode t))
+;; ;; Company -- text completion
+;; (use-package company
+;;   :delight
+;;   :config
+;;   (setq company-idle-delay 0.3
+;;         company-minimum-prefix-length 1
+;;         company-dabbrev-ignore-case nil)
+;;   (global-company-mode t))
 
 ;;; format code helper
 (use-package format-all
