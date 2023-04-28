fd212b343a0c1f0883ea156bdd39d776f775eafc
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2023-04-28 12:28:53 +1000

    move custom settings out of my init.el

---
+++ custom.el
@@ -0,0 +1,8 @@
+;;; custom.el --- Summary  -*- lexical-binding: t; -*-
+;;; Commentary:
+;;;
+;;; All local customisations
+
+;;; Code:
+(provide 'custom)
+;;; custom.el ends here
--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; Package --- Summary  -*- lexical-binding: t; -*-
+;;; init.el --- Summary  -*- lexical-binding: t; -*-
 ;;; Commentary:
 ;;;
 ;;; Reload this config with M-: (load user-init-file)
@@ -123,6 +123,10 @@
       `((".*" . ,emacs-tmp-dir)))
 (setq auto-save-file-name-transforms
       `((".*" ,emacs-tmp-dir t)))
+
+;; Keep customisations out of my config
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -359,18 +363,3 @@
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
-   '((vc-prepare-patches-separately)
-     (diff-add-log-use-relative-names . t)
-     (vc-git-annotate-switches . "-w"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
