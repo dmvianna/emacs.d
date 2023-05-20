e9c64e6e457a17e2017f6b1f9eda27a149df7fc4
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2023-05-20 15:42:38 +1000

    add custom set variables for emacs sources

--- custom.el
+++ custom.el
@@ -6,3 +6,18 @@
 ;;; Code:
 (provide 'custom)
 ;;; custom.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((vc-prepare-patches-separately)
+     (diff-add-log-use-relative-names . t)
+     (vc-git-annotate-switches . "-w"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
