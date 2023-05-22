98e65b42a41764752332dda0cf9e91dbb4f0d08e
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2023-05-22 16:50:01 +1000

    rm redundancy in exec-path-from-shell

--- mixins/dev.el
+++ mixins/dev.el
@@ -137,7 +137,6 @@
 (use-package exec-path-from-shell
   :config
   (dolist (var '("PATH" "SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO"
-                 "SSH_AGENT_PID" "SSH_AUTH_SOCK"
                  "LANG" "LC_CTYPE" "NIX_SSL_CERT_FILE" "NIX_PATH" "XAUTHORITY"))
     (add-to-list 'exec-path-from-shell-variables var))
   (exec-path-from-shell-initialize))
