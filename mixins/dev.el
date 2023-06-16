6ac93559b0ec03221a8d77e417da9f1906015fb2
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2023-06-16 15:12:51 +1000

    move org-related stuff to org-config.el

--- mixins/dev.el
+++ mixins/dev.el
@@ -155,32 +155,6 @@
 ;; rest
 (use-package verb)
 
-;; org-related
-(use-package org-pandoc-import
-  :elpaca (:host github
-                 :repo "tecosaur/org-pandoc-import"
-                 :files ("*.el" "filters" "preprocessors")))
-
-(use-package org-roam)
-
-(use-package org-journal
-  :bind
-  ("C-c n j" . org-journal-new-entry)
-  :custom
-  (org-journal-date-prefix "#+title: ")
-  (org-journal-file-format "%Y-%m-%d.org")
-  (org-journal-dir "~/Documents/Notes/")
-  (org-journal-date-format "%A, %d %B %Y"))
-
-(use-package org-download
-  :after org
-  :bind
-  (:map org-mode-map
-        (("s-Y" . org-download-screenshot)
-         ("s-y" . org-download-yank)))
-  :custom (org-download-method 'attach)
-  :ensure-system-package wl-clipboard)
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;;   CLI
