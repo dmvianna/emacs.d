eb1812746c810635f415a70ac3a28a9226b721e5
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2024-03-12 15:10:20 +1100

    add tramp method to sudo after logging into GCS compute machine

--- mixins/tramp-config.el
+++ mixins/tramp-config.el
@@ -20,6 +20,20 @@
                    ("-o" "StrictHostKeyChecking=no")))
    (tramp-default-port 22)))
 
+(add-to-list
+ 'tramp-methods
+ '("gcsroot"
+   (tramp-login-program "gcloud compute ssh")
+   (tramp-login-args (("%h" "--" "sudo" "su")))
+   (tramp-async-args (("-q")))
+   (tramp-remote-shell "/bin/sh")
+   (tramp-remote-shell-args ("-c"))
+   (tramp-gw-args (("-o" "GlobalKnownHostsFile=/dev/null")
+                   ("-o" "UserKnownHostsFile=/dev/null")
+                   ("-o" "StrictHostKeyChecking=no")))
+   (tramp-default-port 22)))
+
+
 ;; ... after which it's as easy as:
 ;;
 ;; C-x C-f /gcssh:compute-instance:/path/to/filename.smile
