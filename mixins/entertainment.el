9eafa222fbb729314b07654070ae9a4cc8563652
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2024-06-05 11:01:39 +1000

    give emms some love

--- mixins/entertainment.el
+++ mixins/entertainment.el
@@ -6,16 +6,24 @@
 ;; emms -- media player
 
 (use-package emms
+  :custom
+  (emms-source-file-default-directory "~/music/")
+  (emms-playlist-buffer-name "*Music*")
+  (emms-info-asynchronously t)
+  ;;; make sure libtag is the only thing delivering metadata
+  (emms-info-functions '(emms-info-libtag))
+  (buffer-save-without-query t)
   :config
   (require 'emms-setup)
+  ;;; load functions that will talk to emms-print-metadata
+  ;;; which in turn talks to libtag and gets metadata
+  (require 'emms-info-libtag)
+  (require 'emms-mode-line)
   (emms-all)
   (emms-default-players)
-  (setq emms-source-file-default-directory "~/music/")
-  (setq emms-playlist-buffer-name "*Music*")
-  (setq emms-info-asynchronously t)
-  (require 'emms-info-libtag) ;;; load functions that will talk to emms-print-metadata which in turn talks to libtag and gets metadata
-  (setq emms-info-functions '(emms-info-libtag)) ;;; make sure libtag is the only thing delivering metadata
-  (require 'emms-mode-line)
   (emms-mode-line 1)
   (require 'emms-playing-time)
   (emms-playing-time 1))
+
+(provide 'entertainment)
+;;; entertainment.el ends here.
