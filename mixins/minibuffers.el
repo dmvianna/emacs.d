52dc393a889f8ce356c2666eb40be37bd60934b0
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2024-06-25 10:39:38 +1000

    disable cape-file. It is too noisy.

--- mixins/minibuffers.el
+++ mixins/minibuffers.el
@@ -68,7 +68,7 @@
   ;; first function returning a result wins.  Note that the list of buffer-local
   ;; completion functions takes precedence over the global list.
   (add-hook 'completion-at-point-functions #'cape-dabbrev)
-  (add-hook 'completion-at-point-functions #'cape-file)
+  ;; (add-hook 'completion-at-point-functions #'cape-file)
   (add-hook 'completion-at-point-functions #'cape-elisp-block)
   (add-hook 'completion-at-point-functions #'cape-history)
   (add-hook 'completion-at-point-functions #'cape-keyword)
