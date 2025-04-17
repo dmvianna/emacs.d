93954fc6047b8b93c9b29ee3640b234cd73573ff
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2025-04-17 16:17:04 +1000

    use jtsx instead of my handrolled mess

--- mixins/languages/web-config.el
+++ mixins/languages/web-config.el
@@ -3,6 +3,55 @@
 ;;; All things web
 ;;; Code:
 
+(use-package jtsx
+  :mode (("\\.jsx?\\'" . jtsx-jsx-mode)
+         ("\\.tsx\\'" . jtsx-tsx-mode)
+         ("\\.m?ts\\'" . jtsx-typescript-mode))
+  :commands jtsx-install-treesit-language
+  :hook ((jtsx-jsx-mode . hs-minor-mode)
+         (jtsx-tsx-mode . hs-minor-mode)
+         (jtsx-typescript-mode . hs-minor-mode))
+  :custom
+  ;; Optional customizations
+  (js-indent-level 2)
+  (typescript-ts-mode-indent-offset 2)
+  (jtsx-switch-indent-offset 0)
+  (jtsx-indent-statement-block-regarding-standalone-parent nil)
+  (jtsx-jsx-element-move-allow-step-out t)
+  (jtsx-enable-jsx-electric-closing-element t)
+  (jtsx-enable-electric-open-newline-between-jsx-element-tags t)
+  (jtsx-enable-jsx-element-tags-auto-sync nil)
+  (jtsx-enable-all-syntax-highlighting-features t)
+  :config
+  (defun jtsx-bind-keys-to-mode-map (mode-map)
+    "Bind keys to MODE-MAP."
+    (define-key mode-map (kbd "C-c C-j") 'jtsx-jump-jsx-element-tag-dwim)
+    (define-key mode-map (kbd "C-c j o") 'jtsx-jump-jsx-opening-tag)
+    (define-key mode-map (kbd "C-c j c") 'jtsx-jump-jsx-closing-tag)
+    (define-key mode-map (kbd "C-c j r") 'jtsx-rename-jsx-element)
+    (define-key mode-map (kbd "C-c <down>") 'jtsx-move-jsx-element-tag-forward)
+    (define-key mode-map (kbd "C-c <up>") 'jtsx-move-jsx-element-tag-backward)
+    (define-key mode-map (kbd "C-c C-<down>") 'jtsx-move-jsx-element-forward)
+    (define-key mode-map (kbd "C-c C-<up>") 'jtsx-move-jsx-element-backward)
+    (define-key mode-map (kbd "C-c C-S-<down>") 'jtsx-move-jsx-element-step-in-forward)
+    (define-key mode-map (kbd "C-c C-S-<up>") 'jtsx-move-jsx-element-step-in-backward)
+    (define-key mode-map (kbd "C-c j w") 'jtsx-wrap-in-jsx-element)
+    (define-key mode-map (kbd "C-c j u") 'jtsx-unwrap-jsx)
+    (define-key mode-map (kbd "C-c j d n") 'jtsx-delete-jsx-node)
+    (define-key mode-map (kbd "C-c j d a") 'jtsx-delete-jsx-attribute)
+    (define-key mode-map (kbd "C-c j t") 'jtsx-toggle-jsx-attributes-orientation)
+    (define-key mode-map (kbd "C-c j h") 'jtsx-rearrange-jsx-attributes-horizontally)
+    (define-key mode-map (kbd "C-c j v") 'jtsx-rearrange-jsx-attributes-vertically))
+
+  (defun jtsx-bind-keys-to-jtsx-jsx-mode-map ()
+      (jtsx-bind-keys-to-mode-map jtsx-jsx-mode-map))
+
+  (defun jtsx-bind-keys-to-jtsx-tsx-mode-map ()
+      (jtsx-bind-keys-to-mode-map jtsx-tsx-mode-map))
+
+  (add-hook 'jtsx-jsx-mode-hook 'jtsx-bind-keys-to-jtsx-jsx-mode-map)
+  (add-hook 'jtsx-tsx-mode-hook 'jtsx-bind-keys-to-jtsx-tsx-mode-map))
+
 (use-package js-ts-mode
   :ensure nil
   :mode (("\\.c?js\\'" . js-ts-mode))
@@ -12,16 +61,9 @@
                 (require 'tide)
                 (tide-setup)))))
 
-(use-package js-jsx-mode
-  :ensure nil
-  :mode (("\\.jsx\\'" . js-jsx-mode))
-  :hook (js-jsx-mode . (lambda ()
-                         (require 'tide)
-                         (tide-setup))))
-
 (use-package web-mode
-  :mode (("\\.html?\\'" . web-mode)
-         ("\\.jsx\\'" . web-mode))
+  :mode (("\\.html?\\'" . web-mode))
+
   :custom
   (web-mode-markup-indent-offset 2)
   (web-mode-css-indent-offset 2)
@@ -32,31 +74,6 @@
   (web-mode-enable-auto-pairing t)
   (web-mode-enable-comment-keywords t)
   (web-mode-enable-current-element-highlight t))
-
-(use-package typescript-mode
-  :init
-  (define-derived-mode typescript-tsx-mode typescript-mode "tsx")
-  :custom
-  (typescript-indent-level 2)
-  :hook
-  ((typescript-mode . subword-mode)
-   (typescript-mode . (lambda ()
-                        (require 'tide)
-                        (tide-setup))))
-  :mode
-  ("\\.tsx\\'" . typescript-tsx-mode)
-  ("\\.m?ts\\'" . typescript-mode))
-
-(use-package tree-sitter
-  :hook
-  ((js-ts-mode . tree-sitter-hl-mode)
-   (typescript-tsx-mode . tree-sitter-hl-mode)))
-
-(use-package tree-sitter-langs
-  :after tree-sitter
-  :config
-  (tree-sitter-require 'tsx)
-  (add-to-list 'tree-sitter-major-mode-language-alist '(typescript-tsx-mode . tsx)))
 
 (use-package tide
   :commands tide-setup
