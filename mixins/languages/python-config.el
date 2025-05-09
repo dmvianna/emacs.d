d815481b9f3f8cbe6e319b5907fd21050bc71ce3
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2025-05-09 12:17:36 +1000

    include default workspace config

--- mixins/languages/python-config.el
+++ mixins/languages/python-config.el
@@ -25,6 +25,14 @@
   :ensure nil
   :config
   (add-to-list 'eglot-server-programs '((python-mode . "pyright")))
+  (eglot-workspace-configuration . "{
+    \"python.analysis.typeChecking\" : true,
+    \"python.analysis.typeCheckingMode\" : \"strict\",
+    \"python.analysis.autoImportCompletions\" : true,
+    \"python.analysis.extraPaths\": [
+      \"/path/to/your/project/src\"
+    ]
+  }")
   :hook (python-mode . eglot-ensure)
   :ensure-system-package (pyright-langserver . "uv tool install pyright"))
 
