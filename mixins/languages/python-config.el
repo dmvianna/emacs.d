b99e08598a6798e9da2aa5ac30caf967afb1ca9b
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2025-05-09 11:26:27 +1000

    point to python-ts-mode; comment out lsp-jedi

--- mixins/languages/python-config.el
+++ mixins/languages/python-config.el
@@ -14,25 +14,25 @@
 
 (use-package python-black
   :after python
-  :hook (python-mode . python-black-on-save-mode))
+  :hook (python-ts-mode . python-black-on-save-mode))
 
-(use-package lsp-jedi
-  :config
-  (with-eval-after-load "lsp-mode"
-    (add-to-list 'lsp-disabled-clients 'pyls))
-  ;; (add-to-list 'lsp-enabled-clients 'jedi)
-  (require 'lsp-diagnostics)
-  (lsp-diagnostics-flycheck-enable)
-  :hook (python-mode . (lambda ()
-                         (require 'lsp-jedi)
-                         (lsp)
-                         (poetry-tracking-mode)
-                         (flycheck-add-next-checker
-                          'lsp
-                          'python-pyright)
-                         (flycheck-add-next-checker
-                          'python-pyright
-                          'python-pylint))))
+;; (use-package lsp-jedi
+;;   :config
+;;   (with-eval-after-load "lsp-mode"
+;;     (add-to-list 'lsp-disabled-clients 'pyls))
+;;   ;; (add-to-list 'lsp-enabled-clients 'jedi)
+;;   (require 'lsp-diagnostics)
+;;   (lsp-diagnostics-flycheck-enable)
+;;   :hook (python-mode . (lambda ()
+;;                          (require 'lsp-jedi)
+;;                          (lsp)
+;;                          (poetry-tracking-mode)
+;;                          (flycheck-add-next-checker
+;;                           'lsp
+;;                           'python-pyright)
+;;                          (flycheck-add-next-checker
+;;                           'python-pyright
+;;                           'python-pylint))))
 
 (provide 'python-config)
 ;;; python-config.el ends here
