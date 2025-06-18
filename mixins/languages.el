8ffc5c8a62d588ecc945b41999ec383e95fdf37c
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2025-06-18 19:38:28 +1000

    display-line-numbers-mode in markdown-mode

--- mixins/languages.el
+++ mixins/languages.el
@@ -215,6 +215,7 @@
   :hook
   (markdown-mode . display-line-numbers-mode)
   (markdown-mode . auto-fill-mode)
+  (markdown-mode . display-line-numbers-mode)
   :mode "\\.md$\\|\\.markdown\\|\\.udon$"
   :interpreter "markdown-mode"
   :hook flyspell)
